b712d0fcdd1911bff1740395f8db63602ace216f
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-05-01 13:28:37 +0200

    Add project files.

---
+++ AppBlomsterButik/BlomstViewModel.cs
@@ -0,0 +1,50 @@
+﻿using BlomstViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBlomsterButik
+{
+    public class BlomstViewModel
+    {
+        private string navnBlomst;
+        private int antalBlomst;
+        private string farveBlomst;
+
+        public ObservableCollection<OrdreBlomst> OC_blomster { get; set; }
+
+        public string NavnBlomst { get => navnBlomst; set => navnBlomst = value; }
+        public int AntalBlomst { get => antalBlomst; set => antalBlomst = value; }
+        public string FarveBlomst { get => farveBlomst; set => farveBlomst = value; }
+
+        public RelayCommand AddNyBlomst { get; set; }
+
+        public BlomstViewModel()
+        {
+            OC_blomster = new ObservableCollection<OrdreBlomst>();
+
+            //Testdata 
+            OC_blomster.Add(new OrdreBlomst("Tulipan", 4, "Rød"));
+            OC_blomster.Add(new OrdreBlomst("Tulipan", 3, "Hvid"));
+            OC_blomster.Add(new OrdreBlomst("Tulipan", 2, "Gul"));
+
+            AddNyBlomst = new RelayCommand(AddBlomst);
+
+        }
+
+        /// <summary>
+        /// metode til at tilføje en ny ordreblomst til listen
+        /// </summary>
+        public void AddBlomst()
+        {
+            OrdreBlomst oBlomst = new OrdreBlomst(NavnBlomst, AntalBlomst, FarveBlomst);
+
+            OC_blomster.Add(oBlomst);
+        }
+
+
+    }
+}
---
+++ AppBlomsterButik/OrdreBlomst.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBlomsterButik
+{
+   public class OrdreBlomst
+    {
+		private string navn;
+
+		public string Navn
+		{
+			get { return navn; }
+			set { navn = value; }
+		}
+
+		private int antal;
+
+		public int Antal
+		{
+			get { return antal; }
+			set { antal = value; }
+		}
+
+		private string farve;
+
+		public string Farve
+		{
+			get { return farve; }
+			set { farve = value; }
+		}
+
+		public OrdreBlomst(string navn,int antal , string farve)
+		{
+			this.Navn = navn;
+			this.Antal = antal;
+			this.Farve = farve;
+
+		}
+		public OrdreBlomst()
+		{
+
+		}
+
+	}
+}
